5b3ce4f40cfa7a2ca58a286380028e896cdf4eb7
pratik <pratik@joshsoftware.com>
2012-02-27 17:20:06 +0530

    Exception handling added in main class

--- lib/isp_unity.rb
+++ lib/isp_unity.rb
@@ -1,5 +1,18 @@
-require "isp_unity/version"
+require 'json'
+require './isp_unity/isp'
+require './isp_unity/version'
+require './isp_unity/routing_table'
 
 module IspUnity
-  # Your code goes here...
+  
+  ConfigFilePath = '../config/sample.json'
+  RoutingTablePath = '/etc/iproute2/rt_tables'
+
+  class IspUnityException < Exception
+    def initialize(message)
+      message = message['error'] if message.class == Hash
+      super "[IspUnity] #{message}"
+    end
+  end
+
 end
