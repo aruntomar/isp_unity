f0df5fdd702726c46abe596dc6142d175526accd
pratik <pratik@joshsoftware.com>
2012-03-09 19:47:48 +0530

    Routes and rule functions integrated with main class

--- lib/isp_unity.rb
+++ lib/isp_unity.rb
@@ -1,5 +1,8 @@
 require 'json'
 require 'lib/isp_unity_log'
+require 'lib/route/route'
+require 'lib/system_call'
+require 'lib/rule/rule'
 require 'lib/isp_unity/isp'
 require 'lib/isp_unity/version'
 require 'lib/isp_unity/routing_table'
@@ -17,4 +20,16 @@ module IspUnity
     end
   end
 
+end
+
+IspUnity.config
+IspUnityLog.info(I18n.t('file.write'))
+Route.build_commands(IspUnity.isp_config_list)
+IspUnityLog.info(I18n.t('route.build_commands'))
+if SystemCall.execute(Route.commands)
+  IspUnityLog.info(I18n.t('system_call.execute.route.success'))
+  Rule.build_commands(IspUnity.isp_config_list)
+  IspUnityLog.info(I18n.t('rule.build_commands'))
+  SystemCall.execute(Rule.commands)
+  IspUnityLog.info(I18n.t('system_call.execute.rule.success'))
 end
--- lib/system_call.rb
+++ lib/system_call.rb
@@ -1,5 +1,14 @@
 class SystemCall
-  def execute(&block)
-    yield
+  class << self
+    def execute(commands=[])
+      begin
+        commands.each {|cmd| system(cmd)} 
+        return true
+      rescue Exception => e
+        IspUnityLog.debug("#{e}")
+        IspUnityLog.error(I18n.t('system_call.execute.error'))
+        raise IspUnityException.new(I18n.t('system_call.execute.error'))
+      end
+    end
   end
 end
