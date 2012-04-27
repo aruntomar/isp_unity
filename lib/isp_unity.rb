46658bc9e4c4e6a94c5a509405d3a55dc395fc89
pratik <pratik@joshsoftware.com>
2012-04-27 20:23:37 +0530

    FIX# Config file path changes

--- lib/isp_unity.rb
+++ lib/isp_unity.rb
@@ -17,7 +17,7 @@ module IspUnity
   config_file = File.join([File.expand_path(File.dirname(__FILE__)), '..', 'config', 'settings.yml'])
   ENV['GEM_ENV'] ||= 'development'
   PATH = YAML.load_file( config_file )[ENV['GEM_ENV']]['ubuntu']
-  ConfigFilePath =  File.join([File.expand_path(File.dirname(__FILE__)), '..',PATH['config_file_path']])
+  ConfigFilePath =  File.join(PATH['config_file_path'])
   RoutingTablePath = PATH['rt_table_path'].to_s
 
   class IspUnityException < Exception
