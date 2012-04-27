fecd2ea94bff64e5f33a81f879eb1980d89eb67b
Arun Tomar <arun@solutionenterprises.co.in>
2012-04-27 20:08:35 +0530

    Log File path changed

--- lib/isp_unity_log.rb
+++ lib/isp_unity_log.rb
@@ -9,5 +9,5 @@ filename = "ispunity.log"
 IspUnityLog = Logger.new("log")
 IspUnityLog.level = DEBUG
 pf = PatternFormatter.new(:pattern => "%d %l %m")
-IspUnityLog.outputters = FileOutputter.new("IspUnity", :filename => File.join(File.dirname(__FILE__),"..","log",filename), :formatter => pf)
+IspUnityLog.outputters = FileOutputter.new("IspUnity", :filename => File.join("/var","log",filename), :formatter => pf)
  
