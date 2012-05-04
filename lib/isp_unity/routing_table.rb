45db25385aec304eba9069735843b4de4b20e782
sivagollapalli <siva@joshsoftware.com>
2012-05-04 18:50:41 +0530

    Issue:3
    - set isp config lists to empty if config is not there in etc dir
    - Changed the error message

--- lib/isp_unity/routing_table.rb
+++ lib/isp_unity/routing_table.rb
@@ -20,49 +20,50 @@ module IspUnity
           IspUnityLog.error(I18n.t('file.read.error'))
           raise IspUnityException.new(I18n.t('file.read.error'))
         end
-      else
-        puts "Please make ensure that configuration file has copied on /etc dir"
-        IspUnityLog.error('Please make ensure that configuration file has copied on /etc dir')
-      end
 
-      @isp_config_list = [] 
-      no_of_isp = configurations['no_of_isp']
-      isp_list = configurations['isp']
-      $ip_cluster = configurations['public_dns']
+        @isp_config_list = [] 
+        no_of_isp = configurations['no_of_isp']
+        isp_list = configurations['isp']
+        $ip_cluster = configurations['public_dns']
 
-      if no_of_isp
-        isp_list.each do|data|
-          if data['enabled'] == 'true'
-            ip_addr = SystemCall.get_ip(data['interface'].to_s)
-            if ip_addr
-              data['ip_address'] = SystemCall.get_ip(data['interface'])
-              @isp_config_list.push(Isp.new(data)) 
-            else
-              puts I18n.t('no_ip_addr') + data['name']
-              IspUnityLog.error(I18n.t('no_ip_addr') + data['name'] )
+        if no_of_isp
+          isp_list.each do|data|
+            if data['enabled'] == 'true'
+              ip_addr = SystemCall.get_ip(data['interface'].to_s)
+              if ip_addr
+                data['ip_address'] = SystemCall.get_ip(data['interface'])
+                @isp_config_list.push(Isp.new(data)) 
+              else
+                puts I18n.t('no_ip_addr') + data['name']
+                IspUnityLog.error(I18n.t('no_ip_addr') + data['name'] )
+              end
             end
           end
+          IspUnityLog.info('Isp Object succesfully created!')
+        else
+          raise IspUnityException.new(I18n.t('file.enter_isp'))
+          IspUnityLog.error(I18n.t('file.read.no_isp_found'))
         end
-        IspUnityLog.info('Isp Object succesfully created!')
-      else
-        raise IspUnityException.new(I18n.t('file.enter_isp'))
-        IspUnityLog.error(I18n.t('file.read.no_isp_found'))
-      end
 
 
-      routing_table = File.read(RoutingTablePath)
-      IspUnityLog.info(I18n.t('routing_table.read.success'))
-      begin
-        @isp_config_list.each do|isp_list|
-          unless routing_table.include?(isp_list.name)
-            File.open(RoutingTablePath, 'a') {|f| f.write("#{rand(100)}  #{isp_list.name} \n")}
+        routing_table = File.read(RoutingTablePath)
+        IspUnityLog.info(I18n.t('routing_table.read.success'))
+        begin
+          @isp_config_list.each do|isp_list|
+            unless routing_table.include?(isp_list.name)
+              File.open(RoutingTablePath, 'a') {|f| f.write("#{rand(100)}  #{isp_list.name} \n")}
+            end
+            IspUnityLog.info(I18n.t('isp.created'))
           end
-          IspUnityLog.info(I18n.t('isp.created'))
+        rescue Exception => e
+          IspUnityLog.debug("#{e}")
+          IspUnityLog.error(I18n.t('routing_table.read.error'))
+          raise IspUnityException.new(I18n.t('routing_table.read.error'))
         end
-      rescue Exception => e
-        IspUnityLog.debug("#{e}")
-        IspUnityLog.error(I18n.t('routing_table.read.error'))
-        raise IspUnityException.new(I18n.t('routing_table.read.error'))
+      else
+        @isp_config_list = []
+        puts "Please make ensure that configuration file has copied on /etc dir"
+        IspUnityLog.error('Please make ensure that configuration file has copied on /etc dir')
       end
     end
   end
